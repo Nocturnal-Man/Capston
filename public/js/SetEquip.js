d24e23c6f1a767e6dd93e7cdd650ffd2976cb36d
unknown <dktjs682@gmail.com>
2018-06-23 15:00:01 +0900

    git add

--- public/js/SetEquip.js
+++ public/js/SetEquip.js
@@ -14,6 +14,58 @@ function generateChartData() {
         });
     }
 }
+setInterval(AirHum, 2000);
+
+function AirHum() {
+    database.ref('/HUM').on('value', function (snapshot) {
+        var hum = snapshot.val();
+        $('#AirHum').text(hum + "%");
+    });
+}
+setInterval(wal, 4000);
+
+function wal() {
+    console.log("수위값 읽어오기");
+    database.ref('/E').on('value', function(snapshot2) {
+        database.ref('/WAL').on('value', function(snapshot3) {
+            var E = snapshot2.val();
+            var W1 = snapshot3.val();
+            switch (E) {
+                case 1:
+                    if(W1==1){
+                         $('#WAL').text("수위가 높습니다!");
+                        $('#EQIP_ICON').text("warning");
+                        document.getElementById("EQIP_ICON").style.color = "red";
+                        document.getElementById("WAL").style.color = "red";
+                    }
+                    else{
+                        console.log("ERROR");
+                    }
+                    break;
+                case 0:
+                    if (W1==1) {
+                        $('#WAL').text("안전");
+                        $('#EQIP_ICON').text("thumb_up");
+                        document.getElementById("EQIP_ICON").style.color = "#1e96ff";
+                        document.getElementById("WAL").style.color = "black";
+                    }
+                    else{
+                         $('#WAL').text("수위가 낮습니다!");
+                        $('#EQIP_ICON').text("warning");
+                        document.getElementById("EQIP_ICON").style.color = "red";
+                        document.getElementById("WAL").style.color = "red";
+                    }
+                    break;
+                default:
+                    console.log("ERROR");
+                    break;
+            }
+            console.log("E는"+E+"입니다");
+            console.log("W는"+W1+"입니다");
+        });
+
+    });
+}
 
 var chartData = generateChartData();
 
@@ -30,7 +82,7 @@ var chart = AmCharts.makeChart("chartdiv", {
         "id": "g1",
         "fillAlphas": 0.4,
         "valueField": "visits",
-         "balloonText": "<div style='margin:5px; font-size:19px;'>Visits:<b>[[value]]</b></div>"
+        "balloonText": "<div style='margin:5px; font-size:19px;'>Visits:<b>[[value]]</b></div>"
     }],
     "chartScrollbar": {
         "graph": "g1",
@@ -56,7 +108,7 @@ var chart = AmCharts.makeChart("chartdiv", {
     },
     "export": {
         "enabled": true,
-         "dateFormat": "YYYY-MM-DD HH:NN:SS"
+        "dateFormat": "YYYY-MM-DD HH:NN:SS"
     }
 });
 
@@ -85,7 +137,7 @@ function generateChartData() {
         // each time we add one minute
         newDate.setMinutes(newDate.getMinutes() + i);
         // some random number
-        visits += Math.round((Math.random()<0.5?1:-1)*Math.random()*10);
+        visits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
         // add data item to the array
         chartData.push({
             date: newDate,
@@ -96,63 +148,57 @@ function generateChartData() {
 }
 
 var database = firebase.database();
-database.ref('/GHM').on('value', function (snapshot) {
-    var message = snapshot.val();
 
-    var chart1 = AmCharts.makeChart("chartdiv1", {
-        "theme": "light",
-        "type": "gauge",
-        "axes": [{
-            "topTextFontSize": 20,
-            "topTextYOffset": 70,
-            "axisColor": "#31d6ea",
-            "axisThickness": 1,
+
+var chart1 = AmCharts.makeChart("chartdiv1", {
+    "theme": "light",
+    "type": "gauge",
+    "axes": [{
+        "topTextFontSize": 20,
+        "topTextYOffset": 70,
+        "axisColor": "#31d6ea",
+        "axisThickness": 1,
+        "endValue": 100,
+        "gridInside": true,
+        "inside": true,
+        "radius": "50%",
+        "valueInterval": 10,
+        "tickColor": "#67b7dc",
+        "startAngle": -90,
+        "endAngle": 90,
+        "unit": "%",
+        "bandOutlineAlpha": 0,
+        "bands": [{
+            "color": "#0080ff",
             "endValue": 100,
-            "gridInside": true,
-            "inside": true,
-            "radius": "50%",
-            "valueInterval": 10,
-            "tickColor": "#67b7dc",
-            "startAngle": -90,
-            "endAngle": 90,
-            "unit": "%",
-            "bandOutlineAlpha": 0,
-            "bands": [{
-                    "color": "#0080ff",
-                    "endValue": 100,
-                    "innerRadius": "105%",
-                    "radius": "170%",
-                    "gradientRatio": [0.5, 0, -0.5],
-                    "startValue": 0
+            "innerRadius": "105%",
+            "radius": "170%",
+            "gradientRatio": [0.5, 0, -0.5],
+            "startValue": 0
     }, {
-                    "color": "#3cd3a3",
-                    "endValue": 0,
-                    "innerRadius": "105%",
-                    "radius": "170%",
-                    "gradientRatio": [0.5, 0, -0.5],
-                    "startValue": 0
-    },
-                {
-                    "color": "#3dd9c3",
-                    "endValue": 0,
-                    "innerRadius": "105%",
-                    "radius": "170%",
-                    "gradientRatio": [0.5, 0, -0.5],
-                    "startValue": 0
-    }]
-  }],
-        "arrows": [{
-            "alpha": 2,
-            "innerRadius": "35%",
-            "nailRadius": 0,
-            "radius": "170%"
-  }]
-    });
-    setInterval(smoth, 1000);
+            "color": "#3cd3a3",
+            "endValue": 0,
+            "innerRadius": "105%",
+            "radius": "170%",
+            "gradientRatio": [0.5, 0, -0.5],
+            "startValue": 0
 
-    function smoth() {
+   }]
+  }],
+    "arrows": [{
+        "alpha": 1,
+        "innerRadius": "35%",
+        "nailRadius": 0,
+        "radius": "170%"
+  }]
+});
+setInterval(smoth, 2000);
+
+function smoth() {
+    database.ref('/GHM').on('value', function (snapshot) {
+        var message = snapshot.val();
         chart1.arrows[0].setValue(message);
         chart1.axes[0].setTopText(message + "%");
         chart1.axes[0].bands[1].setEndValue(message);
-    }
-});
+    });
+}
